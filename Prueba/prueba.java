8a2bd9cda1c12179343a4a7a1d0945cd900ac608
IvanLzo <i.lazo1@alumnos.santotomas.cl>
2025-11-13 00:19:36 -0300

    Add files via upload
    0.2

--- Prueba/prueba.java
+++ Prueba/prueba.java
@@ -57,9 +57,8 @@ class MainWindow extends JFrame {
 
     private ArrayList<String> lista = new ArrayList<>();
     private DefaultListModel<String> model;
-    private JList<String> ProductoJList=new JList<>();
+    private JList<String> ProductoJList = new JList<>();
     private ArrayList<String> cantidadProductos = new ArrayList<>();
-    
 
     public MainWindow(String usuario) {
         setTitle("Gestor - Usuario: " + usuario);
@@ -99,59 +98,95 @@ class MainWindow extends JFrame {
             lista.add(item);
             model.addElement(item);
             Scanner cantidad = new Scanner(System.in);
-            while (true) { 
-               System.out.println("Agregar cantidad: ");
-            String stock = cantidad.nextLine();
-            try {
-                int valor= Integer.parseInt(stock);
-            
-            
+            while (true) {
+                System.out.println("Agregar cantidad: ");
+                String stock = cantidad.nextLine();
+                try {
+                    int valor = Integer.parseInt(stock);
 
-            if (stock != null && !stock.trim().isEmpty()) {
-                System.out.println("Cantidad agregado: " + stock);
-                cantidadProductos.add(stock);
-                break;
+                    if (stock != null && !stock.trim().isEmpty()) {
+                        System.out.println("Cantidad agregado: " + stock);
+                        cantidadProductos.add(stock);
+                        break;
 
-            }            else {
-                System.err.println("Cantidad no válida");
-        }} catch (NumberFormatException e) {
-                System.err.println("Cantidad no válida");
+                    } else {
+                        System.err.println("Cantidad no válida");
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Cantidad no válida");
+                }
             }
-            }
-             
-            }
-        else {
-            System.err.println( "Item vacío");
-        
+
+        } else {
+            System.err.println("Item vacío");
+
         }
-    
-            }
-        
+
+    }
 
     private void mostrar() {
         if (lista.isEmpty()) {
             JOptionPane.showMessageDialog(this, "La lista está vacía");
         } else {
-            for (int i=0 ; i < lista.size(); i++) {
+            for (int i = 0; i < lista.size(); i++) {
                 System.out.println("Item: " + lista.get(i) + " - Cantidad: " + cantidadProductos.get(i));
             }
-            
+
         }
     }
 
     private void eliminar() {
-    Scanner texto = new Scanner(System.in);
-        System.out.println("Agregar item: ");
+        Scanner texto = new Scanner(System.in);
+        Scanner cantidad = new Scanner(System.in);
+        System.out.println("Item a eliminar: ");
         String item = texto.nextLine();
-        for (int i = 0; i < lista.size(); i++) {
-            if (lista.get(i).equals(item)) {
-                lista.remove(i);
-                cantidadProductos.remove(i);
-                model.removeElement(item);
-                System.out.println("Item eliminado: " + item);
-                return;
+        if (item != null && !item.trim().isEmpty()) {
+            while (true) {
+
+                System.out.println("Cantidad a eliminar: ");
+                String stock = cantidad.nextLine();
+
+                try {
+                    int numero = Integer.parseInt(stock);
+
+                    if (stock != null && !stock.trim().isEmpty() && numero >= 0) {
+                        for (int i = 0; i < lista.size(); i++) {
+                            if (lista.get(i).equals(item)) {
+                                if (Integer.parseInt(cantidadProductos.get(i)) >= numero) {
+                                    int nuevaCantidad = Integer.parseInt(cantidadProductos.get(i)) - numero;
+                                    if (nuevaCantidad == 0) {
+                                        lista.remove(i);
+                                        cantidadProductos.remove(i);
+                                        model.remove(i);
+                                        System.out.println("Item eliminado completamente: " + item);
+                                    } else {
+                                        cantidadProductos.set(i, String.valueOf(nuevaCantidad));
+                                        System.out.println("Cantidad actualizada para " + item + ": " + nuevaCantidad);
+                                    }
+                                } else {
+                                    System.err.println("No hay suficiente cantidad para eliminar");
+
+                                }
+                                return;
+                            }
+                                
+                                
+                            
+
+                        }
+                    } else {
+                        System.out.println("Item no encontrado: " + item);
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Cantidad no válida");
+                }
             }
-        }System.out.println("Item no encontrado: " + item);
+        } else {
+            System.err.println("Item vacío");
+            return;
+        }
+
     }
 
     private void limpiar() {
